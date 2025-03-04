d204be1a694aba4e926560a3a0cef38b8b22427b
Bibishan Pandey <bibishan.b.pandey@gmail.com>
2025-03-03 22:16:12 -0500

    Add SFML library and link it in CMake

--- AsteroidMiningSimulation/AsteroidMiningSimulationGame/src/main.cpp
+++ AsteroidMiningSimulation/AsteroidMiningSimulationGame/src/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 
+#include "SFML/Graphics.hpp"
+
 int main() {
+    sf::RenderWindow window(sf::VideoMode({100, 100}), "Asteroid Mining Simulation");
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
